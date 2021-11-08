8c6d9c4e0b565de36710917c3d7f3b7235269182
Parker <parkerlockhart42@gmail.com>
2021-11-08 12:01:24 -0600

    Add craft_with_most_supplies test

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,30 +1,43 @@
 require './lib/person'
+require './lib/craft'
+require './lib/event'
 
 describe Person do
   before(:each) do
-    @person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    @hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    @sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
   end
 
   describe '#initialize' do
     it 'exists' do
-      expect(@person).to be_instance_of(Person)
+      expect(@hector).to be_instance_of(Person)
     end
 
     it 'has a name' do
-      expect(@person.name).to eq('Hector')
+      expect(@hector.name).to eq('Hector')
     end
 
     it 'has interests' do
-      expect(@person.interests).to eq(['sewing', 'millinery', 'drawing'])
+      expect(@hector.interests).to eq(['sewing', 'millinery', 'drawing'])
     end
   end
 
   describe '#add_supply' do
     it 'adds supplies' do
-      @person.add_supply('fabric', 4)
-      @person.add_supply('scissors', 1)
-      @person.add_supply('fabric', 3)
-      expect(@person.supplies).to eq({"fabric"=>7, "scissors"=>1})
-    end 
+      @hector.add_supply('fabric', 4)
+      @hector.add_supply('scissors', 1)
+      @hector.add_supply('fabric', 3)
+      expect(@hector.supplies).to eq({"fabric"=>7, "scissors"=>1})
+    end
+  end
+
+  describe '#can_build?' do
+    it 'returns a boolean based on supplies' do
+      @hector.add_supply('fabric', 7)
+      @hector.add_supply('thread', 1)
+      @hector.add_supply('scissors', 1)
+      @hector.add_supply('sewing_needles', 1)
+      expect(@hector.can_build?(@sewing)).to eq(false)
+    end
   end
 end
