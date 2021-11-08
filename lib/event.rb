6b2c8ce744af32046b39b915008424338bc14970
David Kassin <david.j.kassin@gmail.com>
2021-11-08 09:48:03 -0700

    feat: Add craft_with_most_supplies method

--- lib/event.rb
+++ lib/event.rb
@@ -1,12 +1,30 @@
 class Event
 
   attr_reader :name,
-              :craft,
-              :person
+              :crafts,
+              :attendees
 
-  def initialize(name, craft, person)
+  def initialize(name, craft, attendees)
     @name = name
-    @craft = craft
-    @person = person
-  end 
+    @crafts = craft
+    @attendees = attendees
+  end
+
+  def attendee_names
+    names = []
+    @attendees.each do |attendee|
+      names.append(attendee.name)
+    end
+    names
+  end
+
+  def craft_with_most_supplies
+    supply_count = Hash.new
+    @crafts.each do |craft|
+      supply_count[craft.name] = craft.supplies_required.count
+    end
+    supply_count.key(supply_count.values.max)
+  end
+
+
 end
