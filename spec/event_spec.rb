5342027ec6d342eb2fd05c4cee722ac9a92c9d96
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2021-11-08 09:46:02 -0700

    Test: Initialize

---
+++ spec/event_spec.rb
@@ -0,0 +1,19 @@
+require './lib/person'
+require './lib/craft'
+require './lib/event'
+
+RSpec.describe Event do
+  let!(:craft){Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})}
+  let!(:person){Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})}
+  let!(:event){Event.new("Carla's Craft Connection", [craft], [person])}
+  describe '#initialize' do
+      it "exists" do
+        expect(event).to be_a Event
+      end
+
+      it "has attributes" do
+        expect(event.name).to eq "Carla's Craft Connection"
+        expect(event.crafts).to eq [craft]
+        expect(event.attendees).to eq [person]
+      end
+  end
