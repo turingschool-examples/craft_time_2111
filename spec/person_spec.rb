7188bd57e47df1cd35998116d2c3490317eb99ae
Brad Breiten <jonathan@breiten.us>
2021-11-08 09:27:31 -0700

    Test: add test for add_supply oteration pattern

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,13 +1,9 @@
 require './lib/person'
-require './lib/craft'
-require './lib/event'
-
 
 RSpec.describe Person do
 
   let(:person) {Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})}
 
-  let(:craft) {Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})}
 
   describe '#initialize' do
     it 'exists' do
@@ -23,12 +19,23 @@ RSpec.describe Person do
     end
 
     it 'has supplies' do
-      expect(person.supplies).to eq {}
+      expect(person.supplies).to eq({})
     end
   end
 
   describe '#add_supply' do
     it 'adds argument to supplies hash' do
+      person.add_supply('fabric', 4)
+      person.add_supply('scissors', 1)
+
+      expect(person.supplies).to eq({"fabric"=>4, "scissors"=>1})
+    end
+
+    it 'adds more of argument to supplies hash' do
+      person.add_supply('fabric', 4)
+      person.add_supply('scissors', 1)
+      person.add_supply('fabric', 3)
+
       expect(person.supplies).to eq({"fabric"=>7, "scissors"=>1})
     end
   end
