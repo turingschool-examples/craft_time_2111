3ba291d9d2b5b6b69e9b32099ad609d0d6a35c71
Joel Grant <joelmgrant@me.com>
2021-11-08 09:47:36 -0700

    Resolve typos

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -42,16 +42,23 @@ describe Event do
     end
   end
 
-  before(:each) do
-    @hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
-    @toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
-    @sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
-    @knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
-    @event = Event.new("Carla's Craft Connection", [@sewing, @knitting], [@hector, @toni])
-  end
+  # before(:each) do
+  #   @hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+  #   @toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+  #   @sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+  #   @knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+  #   @event = Event.new("Carla's Craft Connection", [@sewing, @knitting], [@hector, @toni])
+  # end
 
   describe '#attendee_names' do
     it 'returns the name of the attendees' do
-      expect(@event.atendee_names).to eq(["Hector", "Toni"])
+      @hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+      @toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+      @sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+      @knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+      @event = Event.new("Carla's Craft Connection", [@sewing, @knitting], [@hector, @toni])
+      expect(@event.attendee_names).to eq(["Hector", "Toni"])
+    end
+  end
 
 end
