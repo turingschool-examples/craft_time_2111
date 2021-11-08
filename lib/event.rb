336216849e65c06acf178162d00f4ab8568b6d44
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-11-08 09:59:37 -0700

    Feat: #craft_with_most_supplies is passing tests

--- lib/event.rb
+++ lib/event.rb
@@ -8,4 +8,12 @@ class Event
     @crafts = [craft].flatten
     @attendees = [attendee].flatten
   end
+
+  def attendee_names
+    @attendees.map {|person| person.name}
+  end
+
+  def craft_with_most_supplies
+    @crafts.group_by {|craft| craft.supplies_required.length}.max[1].map{|craft| craft.name}[0]
+  end
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -7,18 +7,37 @@ RSpec.describe Event do
     let(:person) {Person.new({
       name: 'Hector',
       interests: ['sewing', 'millinery', 'drawing']})}
+    let(:hector) {Person.new({
+      name: 'Hector',
+      interests: ['sewing', 'millinery', 'drawing']})}
+    let(:toni) {Person.new({
+      name: 'Toni',
+      interests: ['sewing', 'knitting']})}
 
     let(:craft) {Craft.new('knitting', {
       yarn: 20,
       scissors: 1,
       knitting_needles: 2})}
+    let(:sewing) {Craft.new('sewing', {
+      fabric: 5,
+      scissors: 1,
+      thread: 1,
+      sewing_needles: 1})}
+    let(:knitting) {Craft.new('knitting', {
+      yarn: 20,
+      scissors: 1,
+      knitting_needles: 2})}
+
 
     let(:event) {Event.new("Carla's Craft Connection", [craft], [person])}
+    let(:event_2) {Event.new("Carla's Craft Connection", [sewing, knitting], [hector, toni])}
+
 
     describe 'Event' do
       it 'exists' do
 
         expect(event).to be_a(Event)
+        expect(event_2).to be_a(Event)
       end
 
       it 'has attributes' do
@@ -26,6 +45,34 @@ RSpec.describe Event do
         expect(event.name).to eq("Carla's Craft Connection")
         expect(event.crafts).to eq([craft])
         expect(event.attendees).to eq([person])
+
+        expect(event_2.name).to eq("Carla's Craft Connection")
+        expect(event_2.crafts).to eq([sewing, knitting])
+        expect(event_2.attendees).to eq([hector, toni])
+      end
+    end
+
+    describe 'Event_2' do
+      it 'exists' do
+
+        expect(event_2).to be_a(Event)
+      end
+
+      it 'has attributes' do
+
+        expect(event_2.name).to eq("Carla's Craft Connection")
+        expect(event_2.crafts).to eq([sewing, knitting])
+        expect(event_2.attendees).to eq([hector, toni])
+      end
+
+      it 'can display attendee names' do
+
+        expect(event_2.attendee_names).to eq(["Hector", "Toni"])
+      end
+
+      it 'can select the craft with the most supplies' do
+
+        expect(event_2.craft_with_most_supplies).to eq("sewing")
       end
     end
   end
