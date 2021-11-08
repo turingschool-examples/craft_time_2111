cbf60da499302dd35884c9e2bdd5adc60f68f8c4
Robin Dittrich <robinodittrich@gmail.com>
2021-11-08 08:08:16 -0800

    Create person class.

---
+++ lib/person.rb
@@ -0,0 +1,8 @@
+class Person
+  attr_reader :name, :interests, :supplies
+  def initialize(attributes)
+    @name = attributes[:name]
+    @interests = attributes[:interests]
+    @supplies = {}
+  end
+end
