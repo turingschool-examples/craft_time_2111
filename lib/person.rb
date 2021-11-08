bdbc9fa89588d61509abb43e3359a26ba634c63b
Russell Rockwood <russell.rockwood@gmail.com>
2021-11-08 12:57:36 -0500

    add can_build method

--- lib/person.rb
+++ lib/person.rb
@@ -11,4 +11,23 @@ class Person
   def add_supply(name, count)
     @supplies[name] = count
   end
+
+  def can_build?(craft)
+    supplies_required_names = craft.supplies_required.keys
+    supplies_required_names = supplies_required_names.map { |key| key.to_s  }
+    person_supply_names = @supplies.keys
+
+    if !(supplies_required_names - person_supply_names).empty?
+      return false
+    end
+
+    craft.supplies_required.each do |name, count|
+      # require 'pry'; binding.pry
+      if @supplies[name.to_s] < count
+        return false
+      end
+    end
+
+    true
+  end
 end
