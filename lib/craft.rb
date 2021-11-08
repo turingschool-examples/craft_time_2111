847146e874f30acbd3854cc129603caadcde9ba1
Sam Iavri <sammi80134@gmail.com>
2021-11-08 09:15:05 -0700

    Set up Person spec

---
+++ lib/craft.rb
@@ -0,0 +1,5 @@
+craft = Craft.new('knitting', { yarn: 20, scissors: 1, knitting_needles: 2 })
+
+craft.name
+
+craft.supplies_required
---
+++ spec/person_spec.rb
@@ -0,0 +1,24 @@
+require './lib/person'
+
+
+Rspec.describe 'Person' do
+  it 'exists' do
+  person = Person.new({ name: 'Hector', interests: %w[sewing millinery drawing] })
+  expect(persons).to be_an_instance_of(Person)
+  end
+
+  it 'has a name' do
+    person = Person.new({ name: 'Hector', interests: %w[sewing millinery drawing] })
+  expect(person.name).to eq('Hector')
+  end
+
+  it 'has interests' do
+    person = Person.new({ name: 'Hector', interests: %w[sewing millinery drawing] })
+  expect(person.interests).to eq([sewing millinery drawing])
+  end
+
+  it 'has interests' do
+    person = Person.new({ name: 'Hector', interests: %w[sewing millinery drawing] })
+  expect(person.supplies).to eq({})
+  end
+end
