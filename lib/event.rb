5e6d30a5f09c72cbfe3c24653ea4e7182c4050aa
John Napier <jcee.napier@gmail.com>
2021-11-08 09:24:16 -0700

    Add #attendee_names to event class.

--- lib/event.rb
+++ lib/event.rb
@@ -2,10 +2,14 @@ class Event
   attr_reader :name, 
               :crafts, 
               :attendees
-              
+
   def initialize(name, crafts = [], attendees = [])
     @name      = name 
     @crafts    = crafts
     @attendees = attendees
+  end
+
+  def attendee_names
+    @attendees.map { |attendee| attendee.name }
   end
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -18,3 +18,15 @@ describe Event do
     expect(event.attendees).to eq([person])
   end
 end
+
+describe 'iteration 2' do 
+  let(:event) {Event.new("Carla's Craft Connection", [sewing, knitting], [hector, toni])}
+  let(:hector) {Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})}
+  let(:toni) {Person.new({name: 'Toni', interests: ['sewing', 'knitting']})}
+  let(:sewing) {Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})}
+  let(:knitting) {Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})}
+
+  it '#attendee names' do 
+    expect(event.attendee_names).to eq(["Hector", "Toni"])
+  end
+end
