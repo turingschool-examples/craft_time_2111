94ba75979403418ce8f74ea3c73f98b0fba58969
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-11-08 12:57:18 -0500

    Edit add_supply method

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,13 @@
 class Person
   attr_reader :name, :interests, :supplies
+
   def initialize(info)
     @name = info[:name]
     @interests = info[:interests]
     @supplies = {}
+  end
+
+  def add_supply(supply, amount)
+    supplies[supply] = amount
   end
 end
