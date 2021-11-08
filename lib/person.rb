fa68b14ac0b918f70a942058c59174048d7a682a
Leland Curtis <lelandcurtis88@gmail.com>
2021-11-08 10:13:11 -0700

    Add can_build? method.

--- lib/person.rb
+++ lib/person.rb
@@ -10,4 +10,17 @@ class Person
   def add_supply(name, quantity)
     @supplies[name] += quantity
   end
+
+  def can_build?(craft)
+    supplies_required = craft.supplies_required
+    return false if supplies_required.count > @supplies.keys.count
+    # for each supply required, check if person has it in correct quantity
+    supplies_required.each do |supply, quantity|
+      if @supplies.key?(supply.to_s) && @supplies[supply.to_s] >= quantity
+        return true
+      else
+        return false
+      end
+    end
+  end
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -28,5 +28,21 @@ describe Person do
         expect(@person.supplies).to eq({"fabric"=>7, "scissors"=>1})
       end
     end
+
+    describe ' #can_build?' do
+      before(:each) do
+        @hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+        @sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+      end
+      it 'returns true or false if person has enough supplies' do
+        expect(@hector.can_build?(@sewing)).to eq(false)
+        @hector.add_supply('fabric', 7)
+        @hector.add_supply('thread', 1)
+        expect(@hector.can_build?(@sewing)).to eq(false)
+        @hector.add_supply('scissors', 1)
+        @hector.add_supply('sewing_needles', 1)
+        expect(@hector.can_build?(@sewing)).to eq(true)
+      end
+    end
   end
 end
