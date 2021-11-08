bdc9d17f831196707023a6e0b9970c339b1a81ba
Joel Grant <joelmgrant@me.com>
2021-11-08 09:37:58 -0700

    Fix test errors

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -4,7 +4,9 @@ require './lib/event'
 
 describe Event do
   before(:each) do
-    @event = Event.new("Carla's Craft Connection", [craft], [person])
+    @person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    @craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    @event = Event.new("Carla's Craft Connection", [@craft], [@person])
   end
 
   describe '#initialize' do
@@ -17,19 +19,23 @@ describe Event do
     end
 
     it 'returns the crafts at the event' do
-      expect(@event.name).to eq([craft])
+      expect(@event.name).to eq("Carla's Craft Connection")
     end
 
     it 'returns the attendees' do
-      expect(@event.attendees).to eq([person])
+      expect(@event.attendees).to eq([@person])
     end
   end
 
-  describe '#add_supply & #supplies' do
+  describe '#add_supply' do
     it 'adds supplies and #supplies returns the changes' do
       @person.add_supply('fabric', 4)
       @person.add_supply('scissors', 1)
       expect(@person.supplies).to eq({"fabric"=>4, "scissors"=>1})
+      @person.add_supply('fabric', 3)
+      expect(@person.supplies).to eq({"fabric"=>7, "scissors"=>1})
     end
   end
+
+
 end
