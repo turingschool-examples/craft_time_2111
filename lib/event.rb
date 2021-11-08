19543e4e3c60cf9fec59e7a7af82c009b709633c
Leland Curtis <lelandcurtis88@gmail.com>
2021-11-08 09:52:49 -0700

    Add event methods.

--- lib/event.rb
+++ lib/event.rb
@@ -6,4 +6,18 @@ class Event
     @crafts = crafts
     @attendees = attendees
   end
+
+  def attendee_names
+    attendee_names = @attendees.map{|attendee| attendee.name}
+  end
+
+  def craft_with_most_supplies
+    @crafts.max_by{|craft|craft.supplies_required.keys.count}.name
+  end
+
+  def supply_list
+
+    crafts = @crafts.flat_map{|craft| craft.supplies_required.keys}
+    crafts.uniq!.map{|name| name.to_s}
+  end
 end
