f6a0c40e84c58e584a5479e3896c5d9b2579ca8b
Sam Iavri <sammi80134@gmail.com>
2021-11-08 10:12:17 -0700

    Hector is an instance of Person class

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -2,29 +2,29 @@ require './lib/person'
 
 RSpec.describe 'Person' do
   it 'exists' do
-    person = Person.new({ name: 'Hector', interests: %w[sewing millinery drawing] })
-    expect(person).to be_an_instance_of(Person)
+    hector = Person.new({ name: 'Hector', interests: %w[sewing millinery drawing] })
+    expect(hector).to be_an_instance_of(Person)
   end
 
   it 'has a name' do
-    person = Person.new({ name: 'Hector', interests: %w[sewing millinery drawing] })
-    expect(person.name).to eq('Hector')
+    hector = Person.new({ name: 'Hector', interests: %w[sewing millinery drawing] })
+    expect(hector.name).to eq('Hector')
   end
 
   it 'has interests' do
-    person = Person.new({ name: 'Hector', interests: %w[sewing millinery drawing] })
-    expect(person.interests).to eq(%w[sewing millinery drawing])
+    hector = Person.new({ name: 'Hector', interests: %w[sewing millinery drawing] })
+    expect(hector.interests).to eq(%w[sewing millinery drawing])
   end
 
   it 'has no supplies' do
-    person = Person.new({ name: 'Hector', interests: %w[sewing millinery drawing] })
-    expect(person.supplies).to eq({})
+    hector = Person.new({ name: 'Hector', interests: %w[sewing millinery drawing] })
+    expect(hector.supplies).to eq({})
   end
 
   it 'has supplies' do
-    person = Person.new({ name: 'Hector', interests: %w[sewing millinery drawing] })
-    person.add_supply('fabric', 4)
-    person.add_supply('scissors', 1)
-    expect(person.supplies).to eq({ 'fabric' => 4, 'scissors' => 1 })
+    hector = Person.new({ name: 'Hector', interests: %w[sewing millinery drawing] })
+    hector.add_supply('fabric', 4)
+    hector.add_supply('scissors', 1)
+    expect(hector.supplies).to eq({ 'fabric' => 4, 'scissors' => 1 })
   end
 end
