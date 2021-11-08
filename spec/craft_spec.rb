ba159ba2b909552f5d60edbe89854ddf8e04308a
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-11-08 09:18:20 -0700

    Test: test for intializing Craft class with attributes

---
+++ spec/craft_spec.rb
@@ -0,0 +1,11 @@
+require './lib/craft'
+
+RSpec.describe Craft do
+  it 'exists and has attributes' do
+    craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+
+    expect(craft).to be_an_instance_of(Craft)
+    expect(craft.name).to eq("knitting")
+    expect(craft.supplies_required).to eq({:yarn=>20, :scissors=>1, :knitting_needles=>2})
+  end
+end
