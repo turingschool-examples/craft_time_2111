7806beb1a40fe538e307e694cd50f46c65845fb6
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2021-11-08 10:10:06 -0700

    Feat: supply_list

--- lib/event.rb
+++ lib/event.rb
@@ -9,7 +9,12 @@ class Event
 
   def craft_with_most_supplies
     @crafts.max_by do |craft|
-      craft.supplies_required.count   
+      craft.supplies_required.count
     end
+  end
+
+  def supply_list
+  list = (@crafts.flat_map { |craft| craft.supplies_required.keys  }).uniq
+  list.map { |supply| supply.to_s  }
   end
 end
