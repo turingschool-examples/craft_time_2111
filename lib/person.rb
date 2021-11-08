cba375b4de409cfb90ab560b0cfc06c5bfa10784
Ruben Pasillas <hobbbes@protonmail.com>
2021-11-08 09:07:58 -0700

    Add initialize attributes and values

--- lib/person.rb
+++ lib/person.rb
@@ -2,6 +2,11 @@ class Person
 
   attr_reader :name, :interests, :supplies
 
-  
+  def initialize(inputted_hash)
+    @name = inputted_hash[:name]
+    @interests = inputted_hash[:interests]
+    @supplies = {}
+  end
+
 
 end
