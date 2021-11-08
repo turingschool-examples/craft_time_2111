944736ced10f386fac4069fa1456b04f1d78d1a4
Kevin Gloss <kgloss89@gmail.com>
2021-11-08 09:30:45 -0700

    Test attendee_names

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -4,13 +4,13 @@ require './lib/event'
 
 RSpec.describe Event do
 
-  before(:each) do
-    @craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
-    @person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
-    @event = Event.new("Carla's Craft Connection", [@craft], [@person])
-  end
-
   describe '#initialize' do
+    before(:each) do
+      @craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+      @person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+      @event = Event.new("Carla's Craft Connection", [@craft], [@person])
+    end
+
     it 'exists' do
       expect(@event).to be_an_instance_of(Event)
     end
@@ -19,6 +19,20 @@ RSpec.describe Event do
       expect(@event.name).to eq("Carla's Craft Connection")
       expect(@event.crafts).to eq([@craft])
       expect(@event.attendees).to eq([@person])
+    end
+  end
+
+  describe 'Iteration 2' do
+    before(:each) do
+      @hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+      @toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+      @sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+      @knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+      @event = Event.new("Carla's Craft Connection", [@sewing, @knitting], [@hector, @toni])
+    end
+
+    it '#attendee_names' do
+      expect(@event.attendee_names).to eq(['Hector', 'Toni'])
     end
   end
 end
