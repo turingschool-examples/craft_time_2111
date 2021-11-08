1ce7a23823f66cda8e718639f48e22f8cad778c7
Joel Grant <joelmgrant@me.com>
2021-11-08 11:00:21 -0700

    Submission time

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -63,12 +63,18 @@ describe Event do
 
   describe '#craft_with_most_supplies' do
      it 'returns the craft with the most needed supplies' do
+       @sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+       @knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+       @event = Event.new("Carla's Craft Connection", [@sewing, @knitting], [@hector, @toni])
        expect(@event.craft_with_most_supplies).to eq("sewing")
      end
   end
 
   describe '#supply_list' do
     it 'returns the list of all supplies needed' do
+      @sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+      @knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+      @event = Event.new("Carla's Craft Connection", [@sewing, @knitting], [@hector, @toni])
       expected = ["fabric", "scissors", "thread", "sewing_needles", "yarn", "knitting_needles"]
       expect(@event.supply_list).to eq(expected)
     end
@@ -76,8 +82,31 @@ describe Event do
 
   describe '#can_build?' do
     it 'returns a boolean response if there are enough supplies to do the craft' do
+      @hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
       @sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
-      expected(@event.can_build?(@sewing)).to be false
+      @knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+      @event = Event.new("Carla's Craft Connection", [@sewing, @knitting], [@hector, @toni])
+      expected = ["fabric", "scissors", "thread", "sewing_needles", "yarn", "knitting_needles"]
+      @hector.add_supply('fabric', 7)
+      @hector.add_supply('thread', 1)
+      @hector.add_supply('scissors', 1)
+      @hector.add_supply('sewing_needles', 1)
+      expect(@event.can_build?(@sewing)).to be true
+    end
+  end
+
+  describe '#atendees_by_craft_interest' do
+    it 'returns a boolean response if there are enough supplies to do the craft' do
+      @hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+      @sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+      @knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+      @event = Event.new("Carla's Craft Connection", [@sewing, @knitting], [@hector, @toni])
+      expected = ["fabric", "scissors", "thread", "sewing_needles", "yarn", "knitting_needles"]
+      @hector.add_supply('fabric', 7)
+      @hector.add_supply('thread', 1)
+      @hector.add_supply('scissors', 1)
+      @hector.add_supply('sewing_needles', 1)
+      expect(@event.can_build?(@sewing)).to be true
     end
   end
 
