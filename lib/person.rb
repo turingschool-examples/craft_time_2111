a9f3fc32581a39893e8a5c45f0eb15ac49ed5318
David Kassin <david.j.kassin@gmail.com>
2021-11-08 09:09:25 -0700

    feat: Initialize class Person

---
+++ lib/person.rb
@@ -0,0 +1,12 @@
+class Person
+
+  attr_reader :name,
+              :interests,
+              :supplies
+
+  def initialize(details)
+    @name = details[:name]
+    @interests = details[:interests]
+    @supplies =Hash.new
+  end
+end 
