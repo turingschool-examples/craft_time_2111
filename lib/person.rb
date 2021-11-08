b5d9a2b67a11484ac936121d67eb2cb6462c679f
Katy <m.catherine.harrod@gmail.com>
2021-11-08 09:28:35 -0700

    Add existing supplies to stock

--- lib/person.rb
+++ lib/person.rb
@@ -10,9 +10,18 @@ class Person
 
   # take the first arg and assign it to hash key
   # take the second arg and assign it to value
+
+  # well now we are adding existing supplies.
+  # check if key already exists, then add amount
+  # to existing amount. 
   def add_supply(supply, amount)
-    supplies[supply] = amount
+    if supplies.has_key?(supply)
+      supplies[supply] += amount
+      require "pry"; binding.pry
+    else
+      supplies[supply] = amount
+    end
   end
 
-  
+
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -28,7 +28,11 @@ RSpec.describe Person do
     person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
     person.add_supply('fabric', 4)
     person.add_supply('scissors', 1)
-    
+
     expect(person.supplies).to eq({"fabric"=>4, "scissors"=>1})
+
+    person.add_supply('fabric', 3)
+    
+    expect(person.supplies).to eq({"fabric"=>7, "scissors"=>1})
   end
 end
