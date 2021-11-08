c6749e257f8ca7b14885a9c798ce2bd44d7a5b9f
Kevin Gloss <kgloss89@gmail.com>
2021-11-08 09:42:34 -0700

    Feat supply list method

--- lib/event.rb
+++ lib/event.rb
@@ -14,4 +14,10 @@ class Event
   def craft_with_most_supplies
     @crafts.max_by {|craft| craft.supplies_required.count}.name
   end
+
+  def supply_list
+    @crafts.map do |craft|
+      craft.supplies_required.keys.map {|key| key.to_s}
+    end.flatten.uniq
+  end
 end
