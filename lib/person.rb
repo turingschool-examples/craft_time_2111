0936860acb377977306ed92065f886a087d36305
Sam Iavri <sammi80134@gmail.com>
2021-11-08 09:24:22 -0700

    Person has a name, interests, and no supplies

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,9 @@
 class Person
-  attr_reader :name, :interests
+  attr_reader :name, :interests, :supplies
 
   def initialize(attributes)
     @name = attributes.fetch(:name)
     @interests = attributes.fetch(:interests)
+    @supplies = {}
   end
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -6,17 +6,17 @@ RSpec.describe 'Person' do
     expect(person).to be_an_instance_of(Person)
   end
 
-  xit 'has a name' do
+  it 'has a name' do
     person = Person.new({ name: 'Hector', interests: %w[sewing millinery drawing] })
     expect(person.name).to eq('Hector')
   end
 
-  xit 'has interests' do
+  it 'has interests' do
     person = Person.new({ name: 'Hector', interests: %w[sewing millinery drawing] })
     expect(person.interests).to eq(%w[sewing millinery drawing])
   end
 
-  xit 'has interests' do
+  it 'has interests' do
     person = Person.new({ name: 'Hector', interests: %w[sewing millinery drawing] })
     expect(person.supplies).to eq({})
   end
