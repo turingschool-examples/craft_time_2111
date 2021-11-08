9d153594534af879879f31feb1f7a2302410a8ef
Russell Rockwood <russell.rockwood@gmail.com>
2021-11-08 11:44:24 -0500

    add names and supplies tests

--- lib/event.rb
+++ lib/event.rb
@@ -1,3 +1,10 @@
 class Event
+  attr_accessor :name, :crafts, :attendees
+
+  def initialize(name, crafts, attendees)
+    @name = name
+    @crafts = crafts
+    @attendees = attendees
+  end
 
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -4,9 +4,11 @@ require './lib/event'
 
 describe Craft do
   before(:each) do
-    @person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
-    @craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
-    @event = Event.new("Carla's Craft Connection", [@craft], [@person])
+    @hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    @toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+    @sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+    @knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    @event = Event.new("Carla's Craft Connection", [@sewing, @knitting], [@hector, @toni])
   end
 
   it 'exists' do
@@ -19,13 +21,25 @@ describe Craft do
 
   it 'has crafts' do
     expect(@event.crafts).to be_a(Array)
-    expect(@event.crafts[0]).to eq(@craft)
+    expect(@event.crafts[0]).to eq(@sewing)
   end
 
   it 'has attendees' do
     expect(@event.attendees).to be_a(Array)
-    expect(@event.attendees[0]).to eq(@person)
+    expect(@event.attendees[0]).to eq(@hector)
   end
 
+  it 'has attendees names' do
+    expect(@event.attendee_names).to eq(["Hector", "Toni"])
+  end
 
+  it 'finds craft with most supplies' do
+    expect(@event.craft_with_most_supplies).to eq(@sewing)
+  end
+
+  it 'has all supplies' do
+    expect(@event.supply_list).to eq(["fabric", "scissors", "thread", "sewing_needles", "yarn", "knitting_needles"])
+  end
+
+  
 end
