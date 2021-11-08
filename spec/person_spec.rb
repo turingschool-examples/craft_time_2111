950fb60e57f717467b3091bbc901c9d49cf00e21
Ruben Pasillas <hobbbes@protonmail.com>
2021-11-08 10:07:34 -0700

    Magic commit (trivial)

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -43,7 +43,7 @@ RSpec.describe Person do
     @person.add_supply('thread', 1)
     @person.add_supply('scissors', 1)
     @person.add_supply('sewing_needles', 1)
-
+    # require 'pry'; binding.pry
     expect(@person.can_build?(@sewing)).to be true
   end
 
