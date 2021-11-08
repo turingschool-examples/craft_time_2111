b4c54f845c92f1b4e519fb05905aa6403a57ea41
Kevin Gloss <kgloss89@gmail.com>
2021-11-08 09:12:42 -0700

    Create intialize tests for event

---
+++ spec/event_spec.rb
@@ -0,0 +1,24 @@
+require './lib/person'
+require './lib/craft'
+require './lib/event'
+
+RSpec.describe Event do
+
+  before(:each) do
+    @craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    @person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    @event = Event.new("Carla's Craft Connection", [@craft], [@person])
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(@event).to be_an_instance_of(Event)
+    end
+
+    it 'has attributes' do
+      expect(@event.name).to eq("Carla's Craft Connection")
+      expect(@event.crafts).to eq([@craft])
+      expect(@event.attendees).to eq([@person])
+    end
+  end
+end
