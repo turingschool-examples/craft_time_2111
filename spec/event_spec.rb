0b90fb1d5955fc47f52905312b752f46f3feac27
Ruben Pasillas <hobbbes@protonmail.com>
2021-11-08 10:37:34 -0700

    Change #attendees_by_craft_interest from symbols to carrots

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -6,8 +6,10 @@ RSpec.describe Event do
   before(:each) do
     @hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
     @toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+
     @sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
     @knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+
     @event = Event.new("Carla's Craft Connection", [@sewing, @knitting], [@hector, @toni])
   end
 
@@ -37,7 +39,13 @@ RSpec.describe Event do
 
   it 'can return all supplies require for all events' do
     expected_array = ["fabric", "scissors", "thread", "sewing_needles", "yarn", "knitting_needles"]
-    expect(event.supply_list).to eq(expected_array)
+    expect(@event.supply_list).to eq(expected_array)
+  end
+
+  it 'can return #attendees_by_craft_interest' do
+    expected_hash = {"sewing" => [@hector, @toni], "knitting" => [@toni]}
+
+    expect(@event.attendees_by_craft_interest).to eq(expected_hash)
   end
 
 end
