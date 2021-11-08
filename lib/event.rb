622d49fccd7616a5f27a50b36a6b70ff6d2af553
Ruben Pasillas <hobbbes@protonmail.com>
2021-11-08 09:44:47 -0700

    Create #supply_list method

--- lib/event.rb
+++ lib/event.rb
@@ -15,4 +15,18 @@ class Event
 
     attendee_name_array
   end
+
+  def supply_list
+    all_craft_supplies = @crafts.map do |craft|
+      craft.required_supplies.keys
+    end
+
+    all_craft_supplies = all_craft_supplies.uniq.flatten
+
+    supply_list = all_craft_supplies.map do |key|
+      key.to_s
+    end
+
+    supply_list = supply_list.uniq
+  end
 end
