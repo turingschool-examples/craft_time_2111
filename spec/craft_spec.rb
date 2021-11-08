289c42eb00c05d5d8b221d343277840565fb608c
David Kassin <david.j.kassin@gmail.com>
2021-11-08 09:15:50 -0700

    feat: Show a Craft exists and can be initialized

---
+++ spec/craft_spec.rb
@@ -0,0 +1,10 @@
+require './lib/craft'
+describe Craft do
+  it 'exists' do
+    craft = Craft.new("knitting", {yarn: 20, scissors: 1, knitting_needles: 2})
+
+    expect(craft).to be_a Craft
+    expect(craft.name).to eq ("knitting")
+    expect(craft.supplies_required).to eq ({yarn: 20, scissors: 1, knitting_needles: 2})
+  end
+end
