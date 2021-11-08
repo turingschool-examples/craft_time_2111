b3efb9a82509d228f9dca10fd7d717ad5476a391
Parker <parkerlockhart42@gmail.com>
2021-11-08 11:13:20 -0600

    Add add_supply method

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,19 @@
 class Person
-  attr_reader :name, :interests
+  attr_reader :name, :interests, :supplies
 
   def initialize(info)
     @name = info[:name]
     @interests = info[:interests]
+    @supplies = {}
   end
-end 
+
+  def add_supply(item, qty)
+    if @supplies.keys.include?(item)
+      @supplies[item] += qty
+    else
+      @supplies[item] = qty
+    end
+  end
+
+
+end
