1f56cf4dbaaea682552e74488d5b5f6d0096e57e
Brad Breiten <jonathan@breiten.us>
2021-11-08 09:38:46 -0700

    Test: add test for attendee_names method

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -28,4 +28,24 @@ RSpec.describe Event do
     end
   end
 
+  describe '#Iteration 2 Tests' do
+    before(:each) do
+      @hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+
+      @toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+
+      @sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+
+      @knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+
+      @event = Event.new("Carla's Craft Connection", [@sewing, @knitting], [@hector, @toni])
+    end
+
+    describe '#attendee_names' do
+      it 'returns array of attendee names' do
+        expect(event.attendee_names).to eq(["Hector", "Toni"])
+      end
+    end
+  end
+
 end
