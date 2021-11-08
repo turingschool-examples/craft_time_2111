ee3d31c0488412ed77cb3c85afc8e3318f3f32d9
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2021-11-08 09:25:37 -0700

    Test: add_supplies

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -9,7 +9,16 @@ RSpec.describe Person do
 
       it "has attributes" do
         expect(person.name).to eq "Hector"
-        expect(person.intersts).to eq 'sewing', 'millinery', 'drawing']
+        expect(person.interests).to eq ['sewing', 'millinery', 'drawing']
       end
+  end
+  describe '#add_supplies' do
+    it "adds items and their quantities to the supplies hash" do
+      person.add_supply('fabric', 4)
+      person.add_supply('scissors', 1)
+      expect(person.supplies).to eq({"fabric"=>4, "scissors"=>1})
+      person.add_supply('fabric', 3)
+      expect(person.supplies).to eq({"fabric"=>7, "scissors"=>1})
+    end
   end
 end
