5224665a9a3b7ce21ce5734f9086bef3a3cd6c27
Ruben Pasillas <hobbbes@protonmail.com>
2021-11-08 09:33:38 -0700

    Add additional event_spec tests

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -4,9 +4,11 @@ require './lib/event'
 
 RSpec.describe Event do
   before(:each) do
-    @person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
-    @craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
-    @event = Event.new("Carla's Craft Connection", [@craft], [@person])
+    @hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    @toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+    @sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+    @knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    @event = Event.new("Carla's Craft Connection", [@sewing, @knitting], [@hector, @toni])
   end
 
   it 'is an instance of Event' do
@@ -18,11 +20,24 @@ RSpec.describe Event do
   end
 
   it 'can list crafts in a given event' do
-    expect(@event.crafts).to eq([@craft])
+    expect(@event.crafts).to eq([@sewing, @knitting])
   end
 
   it 'can list attendees of a given event' do
-    expect(@event.attendees).to eq([@person])
+    expect(@event.attendees).to eq([@hector, @toni])
+  end
+
+  it 'can return a list of attendee names as an array' do
+    expect(@event.attendee_names).to eq([@hector.name, @toni.name])
+  end
+
+  it 'can return craft with most supplies' do
+    expect(@event.craft_with_most_supplies).to eq("sewing")
+  end
+
+  it 'can return all supplies require for all events' do
+    expected_array = ["fabric", "scissors", "thread", "sewing_needles", "yarn", "knitting_needles"]
+    expect(event.supply_list).to eq(expected_array)
   end
 
 end
