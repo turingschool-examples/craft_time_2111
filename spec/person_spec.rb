30d6f5644561dbb3e0e6df0f20657f54de85d5ed
Jackson Valdez <jacksonvaldez64@gmail.com>
2021-11-08 09:20:02 -0700

    Complete add_supply test in person_spec.rb

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -14,6 +14,16 @@ RSpec.describe Person do
     end
   end
 
+  describe '#add_supply' do
+    it 'adds supplies to the persons supplies attribute' do
+      @person.add_supply('fabric', 4)
+      @person.add_supply('scissors', 1)
+      expect(@person.supplies).to eq({"fabric"=>4, "scissors"=>1})
+      @person.add_supply('fabric', 3)
+      expect(@person.supplies).to eq({"fabric"=>7, "scissors"=>1})
+    end
+  end
+
 
 
 end
