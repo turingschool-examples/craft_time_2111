a503a698e629fa9be1b66a3b7ac71b440a518eee
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2021-11-08 10:01:12 -0700

    Test: craft_with_most_supplies

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -3,9 +3,11 @@ require './lib/craft'
 require './lib/event'
 
 RSpec.describe Event do
-  let!(:craft){Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})}
-  let!(:person){Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})}
-  let!(:event){Event.new("Carla's Craft Connection", [craft], [person])}
+  let!(:knitting){Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})}
+  let!(:sewing){Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})}
+  let!(:hector){Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})}
+  let!(:toni){Person.new({name: 'Toni', interests: ['sewing', 'knitting']})}
+  let!(:event){Event.new("Carla's Craft Connection", [sewing, knitting], [hector, toni])}
   describe '#initialize' do
       it "exists" do
         expect(event).to be_a Event
@@ -13,7 +15,14 @@ RSpec.describe Event do
 
       it "has attributes" do
         expect(event.name).to eq "Carla's Craft Connection"
-        expect(event.crafts).to eq [craft]
-        expect(event.attendees).to eq [person]
+        expect(event.crafts).to eq [sewing, knitting]
+        expect(event.attendees).to eq [hector, toni]
       end
   end
+
+  describe '#craft_with_most_supplies' do 
+    it "finds the craft that needs the most unique items" do
+      expect(event.craft_with_most_supplies).to eq(sewing)
+    end
+  end
+end
