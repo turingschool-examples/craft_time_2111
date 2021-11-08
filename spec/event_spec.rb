9a4e9041be015dca478768e1f37a9c05715c34ba
Mallory Vining <mallorygallery12@gmail.com>
2021-11-08 09:50:26 -0700

    feat: Add test for supply_list

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -28,4 +28,23 @@ describe Event do
     event = Event.new("Carla's Craft Connection", [sewing, knitting], [hector, toni])
     expect(event.attendee_names).to eq(["Hector", "Toni"])
   end
+
+  it '#craft_with_most_supplies' do
+    hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+    sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+    knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    event = Event.new("Carla's Craft Connection", [sewing, knitting], [hector, toni])
+    expect(event.craft_with_most_supplies).to eq("sewing")
+  end
+
+  it '#supply_list' do
+    hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+    sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+    knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    event = Event.new("Carla's Craft Connection", [sewing, knitting], [hector, toni])
+    expected = ["fabric", "scissors", "thread", "sewing_needles", "yarn", "knitting_needles"]
+    expect(event.supply_list).to eq(expected)
+  end
 end
