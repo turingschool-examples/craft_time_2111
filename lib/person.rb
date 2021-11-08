08dabe8f7667c1f5d7d51ec7a05ac1f5f4d10e3e
Joel Grant <joelmgrant@me.com>
2021-11-08 09:38:18 -0700

    Feat: #add_supply

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,13 @@
 class Person
-  attr_reader :name, :interests, :supplies
+  attr_accessor :name, :interests, :supplies
 
   def initialize(attributes)
     @name = attributes[:name]
     @interests = attributes[:interests]
-    @supplies = {}
+    @supplies = Hash.new(0)
+  end
+
+  def add_supply(supply, quantity)
+    @supplies[supply] += quantity
   end
 end
