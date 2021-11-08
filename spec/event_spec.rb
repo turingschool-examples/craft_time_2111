c53d5dc45336e308e8f87fdf904d1d2e9d294072
Brad Breiten <jonathan@breiten.us>
2021-11-08 10:43:45 -0700

    Add tests for crafts_that_use method

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -59,4 +59,28 @@ RSpec.describe Event do
       end
     end
   end
+
+  describe 'Iteration 3 Tests' do
+    before(:each) do
+      @hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+
+      @toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+
+      @tony = Person.new({name: 'Tony', interests: ['drawing', 'knitting']})
+
+      @knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+
+      @sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1})
+
+      @painting = Craft.new('painting', {canvas: 1, paint_brush: 2, paints: 5})
+
+      @event = Event.new("Carla's Craft Connection", [@knitting, @painting, @sewing], [@hector, @toni, @tony])
+    end
+
+    describe '#crafts_that_use' do
+      it 'returns array of crafts if supplies_required includes passed argument' do
+        expect(@event.crafts_that_use("scissors")).to eq([@knitting, @sewing])
+      end
+    end
+  end
 end
