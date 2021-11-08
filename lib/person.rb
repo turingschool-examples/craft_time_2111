f528a496effc8b62322964fb03836b7267798797
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2021-11-08 09:32:49 -0700

    Feat: add_supply

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  attr_reader :name, :interests, :supplies
+
+  def initialize(info_hash)
+    @name = info_hash[:name]
+    @interests = info_hash[:interests]
+    @supplies = {}
+  end
+
+  def add_supply(item, quantity)
+    supplies[item] ||= 0
+    supplies[item] += quantity
+  end
+end
