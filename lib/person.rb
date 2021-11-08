9e25f9414a33288b275e08bd3fa36efc644839b2
Joel Grant <joelmgrant@me.com>
2021-11-08 09:07:28 -0700

    Create Person class and initialize

---
+++ lib/person.rb
@@ -0,0 +1,9 @@
+class Person
+  attr_reader :name, :interests, :supplies
+
+  def initialize(attributes)
+    @name = attributes[:name]
+    @interests = attributes[:interests]
+    @supplies = {}
+  end
+end
