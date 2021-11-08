3d5fc2e11b51df04ca35c2d3768be46bc13f9ef2
Kevin Gloss <kgloss89@gmail.com>
2021-11-08 09:49:17 -0700

    Test can_build?

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,9 +1,11 @@
 require './lib/person'
+require './lib/craft'
 
 RSpec.describe Person do
 
   before(:each) do
     @person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    @sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
   end
 
   describe '#initialize' do
@@ -28,6 +30,20 @@ RSpec.describe Person do
       @person.add_supply('fabric', 3)
 
       expect(@person.supplies).to eq({'fabric' => 7, 'scissors' => 1})
+    end
+  end
+
+  describe '#can_build?' do
+    it 'returns true if the person has enough supplies' do
+      @person.add_supply('fabric', 7)
+      @person.add_supply('thread', 1)
+
+      expect(@person.can_build?(@sewing)).to be false
+
+      @person.add_supply('scissors', 1)
+      @person.add_supply('sewing_needles', 1)
+
+      expect(@person.can_build?(@sewing)).to be true
     end
   end
 end
