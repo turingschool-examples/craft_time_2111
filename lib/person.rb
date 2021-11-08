ca92ec194829b792422034c707839c6973f0c23a
Brad Breiten <jonathan@breiten.us>
2021-11-08 10:39:41 -0700

    Comment out can_build? method. Skipping for now

--- lib/person.rb
+++ lib/person.rb
@@ -11,7 +11,23 @@ class Person
   end
 
   def add_supply(supply, amount)
-    @supplies[supply] += amount 
+    @supplies[supply] += amount
   end
+
+  # def can_build?(craft)
+  #   h1 = craft.supplies_required
+  #   h2 = supplies
+  #
+  #   if h1.length != h2.length
+  #     false
+  #   elsif h1.keys != h2.keys
+  #     false
+  #   elsif
+  #     h1.values.each do |h1value|
+  #        h2.values.each do |h2value|
+  #
+  #     true
+  #   end
+  # end
 
 end
