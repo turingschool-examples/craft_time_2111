58de0af8f6b8a4250e7b58a59120e24b0fd1a006
Sam Iavri <sammi80134@gmail.com>
2021-11-08 09:50:02 -0700

    Event has crafts and attendees

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -13,13 +13,13 @@ RSpec.describe 'Event' do
     expect(event.name).to eq("Carla's Craft Connection")
   end
 
-  xit 'has crafts' do
+  it 'has crafts' do
     event = Event.new("Carla's Craft Connection", [craft], [person])
     craft = Craft.new('knitting', { yarn: 20, scissors: 1, knitting_needles: 2 })
     expect(event.crafts).to be_an_instance_of(Craft)
   end
 
-  xit 'has attendees' do
+  it 'has attendees' do
     event = Event.new("Carla's Craft Connection", [craft], [person])
     person = Person.new({ name: 'Hector', interests: %w[sewing millinery drawing] })
     expect(event.attendees).to be_an_instance_of(Person)
