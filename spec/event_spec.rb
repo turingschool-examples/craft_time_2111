a80ccfdbabbbe94468e262248f91900521bdba50
David Kassin <david.j.kassin@gmail.com>
2021-11-08 09:47:37 -0700

    test: Returns the name of hte craft that requies the most supply types

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -10,7 +10,27 @@ describe Event do
 
     expect(event).to be_a Event
     expect(event.name).to eq ("Carla's Craft Connection")
-    expect(event.craft).to eq (craft)
-    expect(event.person).to eq (person)
+    expect(event.crafts).to eq (craft)
+    expect(event.attendees).to eq (person)
+  end
+
+  it 'returns names of attendees' do
+    hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+    sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+    knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    event = Event.new("Carla's Craft Connection", [sewing, knitting], [hector, toni])
+
+    expect(event.attendee_names).to eq (["Hector", "Toni"])
+  end
+
+  it 'returns the name of the craft that requires the most supply types' do
+    hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+    sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+    knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    event = Event.new("Carla's Craft Connection", [sewing, knitting], [hector, toni])
+
+    expect(event.craft_with_most_supplies).to eq ("sewing")
   end
 end
