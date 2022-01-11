1fee0fe64260e8da8104e5fe4f7ca1a5cbfcf77d
TJ Williams <williamsjr.timothy1@gmail.com>
2022-01-11 12:47:11 -0500

    Add iteration-2 Person test

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -5,7 +5,8 @@ require './lib/event'
 
 RSpec.describe Person do
   subject { Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']}) }
-  
+  let(:sewing) { Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1}) }
+
   it "exists" do
     expect(subject).to be_a Person
     expect(subject.name).to eq('Hector')
@@ -21,5 +22,17 @@ RSpec.describe Person do
     expect(subject.supplies).to eq({"fabric"=>7, "scissors"=>1})
   end
 
+  it "shows if person #can_build?" do
 
+    expect(subject.can_build?(sewing)).to be false
+
+    subject.add_supply('fabric', 7)
+    subject.add_supply('thread', 1)
+    expect(subject.can_build?(sewing)).to be false
+
+    subject.add_supply('scissors', 1)
+    subject.add_supply('sewing_needles', 1)
+  
+    expect(subject.can_build?(sewing)).to be true
+  end
 end
