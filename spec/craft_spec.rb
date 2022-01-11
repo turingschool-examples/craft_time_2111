6f5ebe0565546ef18f6facf8ca132ba72ea5c1a3
Jesse Ochs <91091690+JesseOchs@users.noreply.github.com>
2022-01-11 09:10:51 -0700

    Feat: craft exists and has attributes

---
+++ spec/craft_spec.rb
@@ -0,0 +1,15 @@
+require './lib/person'
+require './lib/craft'
+require './lib/event'
+
+RSpec.describe Craft do
+
+let(:craft) {Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})}
+
+ it 'exists and has attributes' do
+   expect(craft).to be_a(Craft)
+   expect(craft.name).to eq('knitting')
+   expect(craft.supplies_required).to eq({:yarn=>20, :scissors=>1, :knitting_needles=>2})
+ end
+
+end
