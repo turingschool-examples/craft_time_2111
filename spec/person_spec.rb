9849af3ee0c2c4aeb7e00f54ec39ddf881f4369e
Maximilian Wagner <maw3350@gmail.com>
2022-01-11 12:14:18 -0500

    test:add test for #add_supply

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -13,5 +13,16 @@ RSpec.describe Person do
     expect(person.interests).to eq(["sewing", "millinery", "drawing"])
     expect(person.supplies).to eq({})
   end
+
+  it '#add_supply' do
+
+    expect(person.supplies).to eq({})
+    person.add_supply('fabric', 4)
+    person.add_supply('scissors', 1)
+    expect(person.supplies).to eq({"fabric"=>4, "scissors"=>1})
+    person.add_supply('fabric', 3)
+    expect(person.supplies).to eq({"fabric"=>7, "scissors"=>1})
+
+  end
 end
 end
