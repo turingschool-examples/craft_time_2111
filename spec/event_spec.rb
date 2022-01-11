a86d370357a6e02523d2347064a75f02038e6563
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-11 10:28:43 -0600

    Test: Event konws the craft with the most supplies'

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -30,4 +30,14 @@ RSpec.describe Event do
 
     expect(event.attendee_names).to eq(["Hector", "Toni"])
   end
+
+  it 'can return the craft with the most supplies' do
+    hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+    sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+    knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    event = Event.new("Carla's Craft Connection", [sewing, knitting], [hector, toni])
+
+    expect(event.craft_with_most_supplies).to eq("sewing")
+  end
 end
