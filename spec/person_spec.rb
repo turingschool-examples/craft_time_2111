b39a60c3eb80421783ac22612e64f13c9c87c9b4
Joseph Galvin <jwgalvin@gmail.com>
2022-01-11 09:13:12 -0700

    feat: added first person test to spec, added defi and passed person has interests.

---
+++ spec/person_spec.rb
@@ -0,0 +1,15 @@
+require './lib/person'
+require './lib/craft'
+require './lib/event'
+require 'pry'
+require 'RSpec'
+
+RSpec.describe Person do
+  describe "this will test the Person class" do
+    it "checks the functionality" do
+      person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+      expect(person.name).to eq("Hector")
+      expect(person.interests).to eq(['sewing', 'millinery', 'drawing'])
+    end
+  end
+end
