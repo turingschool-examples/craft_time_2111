dbc3080e355c7754e267e1934303cf8a830d47d7
Greg Flaherty <gregoryjflaherty@gmaul.com>
2022-01-11 11:15:44 -0500

    Test: create it has name and crafts test

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -1,25 +1,29 @@
+require './lib/craft'
 require './lib/event'
+require './lib/person'
 
 
 
 RSpec.describe do Event
   it 'exists' do
+    person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
     event = Event.new("Carla's Craft Connection", [craft], [person])
     expect(event).to be_an_instance_of(Event)
   end
 
   it 'has a name' do
+    person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
     event = Event.new("Carla's Craft Connection", [craft], [person])
-    expect(event).to be_an_instance_of(Event)
+    expect(event.name).to eq("Carla's Craft Connection")
   end
 
-  it 'exists' do
+  it 'has crafts' do
+    person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
     event = Event.new("Carla's Craft Connection", [craft], [person])
-    expect(event).to be_an_instance_of(Event)
+    expect(event.crafs).to eq([craft])
   end
 
-  it 'exists' do
-    event = Event.new("Carla's Craft Connection", [craft], [person])
-    expect(event).to be_an_instance_of(Event)
-  end
 end
