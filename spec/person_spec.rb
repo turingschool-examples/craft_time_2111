2341689a680f287c5f10f4e04d0ba5bc1355455a
Greg Flaherty <gregoryjflaherty@gmaul.com>
2022-01-11 11:18:13 -0500

    Test: create adds supplies tests

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -22,4 +22,19 @@ RSpec.describe do Person
     person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
     expect(person.supplies).to eq({})
   end
+
+  it 'adds supplies' do
+    person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    person.add_supply('fabric', 4)
+    person.add_supply('scissors', 1)
+    expect(person.supplies).to eq({"fabric"=>4, "scissors"=>1})
+  end
+
+  it 'adds more supplies' do
+    person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    person.add_supply('fabric', 4)
+    person.add_supply('scissors', 1)
+    person.add_supply('fabric', 3)
+    expect(person.supplies).to eq({"fabric"=>7, "scissors"=>1})
+  end
 end
