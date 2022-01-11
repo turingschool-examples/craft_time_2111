4e12d1aa5d8f384e1f80cec4b41adc4a442b7724
benistta <92293363+benistta@users.noreply.github.com>
2022-01-11 09:45:55 -0700

    attendee names

--- lib/event.rb
+++ lib/event.rb
@@ -5,7 +5,12 @@ attr_reader :name, :crafts, :attendees
     @name = name
     @crafts = crafts
     @attendees = attendees
+  end
 
-
+  def attendee_names
+    @attendees.map do |attendee|
+      # require "pry"; binding.pry
+      attendee.name
+    end
   end
 end
