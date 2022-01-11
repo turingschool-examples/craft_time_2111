2fc2e99d0d5d20766063f4345250b2dac2966664
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-11 10:27:39 -0600

    Add Event#attendee_names to return names of all attendees

--- lib/event.rb
+++ lib/event.rb
@@ -1,11 +1,17 @@
 class Event
 
   attr_reader :name, :crafts, :attendees
-  
+
   def initialize(name, crafts, attendees)
     @name = name
     @crafts = crafts
     @attendees = attendees
+  end
+
+  def attendee_names
+    names = @attendees.map do |attendee|
+      attendee.name
+    end 
   end
 
 end
