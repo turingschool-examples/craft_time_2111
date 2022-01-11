614bb53ac582c7cdb47b93f94d31ea70c1c59e98
Richard LaBrecque <RichardlaBreque@Gmail.com>
2022-01-11 11:17:34 -0500

    test: craft exists test

---
+++ spec/craft_spec.rb
@@ -0,0 +1,8 @@
+require './lib/craft'
+RSpec.describe Craft do
+  it 'exists' do
+    craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    # binding.pry
+    expect(craft).to be_a(Craft)
+  end
+end
