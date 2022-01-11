4f8006aa3a809c6b81e28916892462d9c9d09968
Sam Smith <samlsmith424@gmail.com>
2022-01-11 09:58:21 -0800

    Test: attendees_by_craft_interest and crafts_that_use

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -31,5 +31,29 @@ RSpec.describe Event do
     it '#supply_list' do
       expect(event.supply_list).to eq(["fabric", "scissors", "thread", "sewing_needles", "yarn", "knitting_needles"])
     end
+
+    describe 'iteration 3' do
+      let(:hector) {Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})}
+      let(:toni) {Person.new({name: 'Toni', interests: ['sewing', 'knitting']})}
+      let(:tony) {Person.new({name: 'Tony', interests: ['drawing', 'knitting']})}
+      let(:knitting) {Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})}
+      let(:sewing) {Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1})}
+      let(:painting) {Craft.new('painting', {canvas: 1, paint_brush: 2, paints: 5})}
+      let(:event) {Event.new("Carla's Craft Connection", [knitting, painting, sewing], [hector, toni, tony])}
+
+      it '#attendees_by_craft_interest' do
+        expected = {
+          "knitting" => [toni, tony],
+          "painting" => [],
+          "sewing" => [hector, toni]
+        }
+        expect(event.attendees_by_craft_interest).to eq(expected)
+      end
+
+      it 'can sort crafts that use specific materials' do
+        expect(event.crafts_that_use('scissors')).to eq([knitting, sewing])
+        expect(event.crafts_that_use('fire')).to eq([])
+      end
+    end
   end
 end
