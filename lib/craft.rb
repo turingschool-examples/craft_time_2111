c9a9678e636f31d0de24af8fa882a67665d19b8b
Maximilian Wagner <maw3350@gmail.com>
2022-01-11 12:24:36 -0500

    fix: craft class passes, simplified the initialize. Let hash be implied instead of explicit

--- lib/craft.rb
+++ lib/craft.rb
@@ -3,8 +3,6 @@ class Craft
 
   def initialize(name, supplies_required)
     @name = name
-    @yarn = supplies_required[:yarn]
-    @scissors = supplies_required[:scissors]
-    @knitting_needles = supplies_required[:knitting_needles]
+    @supplies_required = supplies_required
   end
 end
