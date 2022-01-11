08d075feda99285c9ea477fa56d431e6c40b631f
Maximilian Wagner <maw3350@gmail.com>
2022-01-11 12:09:18 -0500

    feat: add event class, does not look right but passes interaction pattern. (attributes look like they need to be put in array?)

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -6,13 +6,14 @@ require 'rspec'
 RSpec.describe Event do
 
   let(:event) {Event.new("Carla's Craft Connection", [craft], [person])}
-
+  let(:craft) {Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})}
+  let(:person) {Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})}
   context 'iteration I' do
 
     it 'exists and has attributes' do
 
       expect(event.name).to eq("Carla's Craft Connection")
-      expect (event.crafts).to eq([craft])
+      expect(event.crafts).to eq([craft])
       expect(event.attendees).to eq([person])
     end
   end
