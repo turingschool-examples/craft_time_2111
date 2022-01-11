fb90c966007e258fae8868cd357de5c0129edef9
Conor <>
2022-01-11 09:14:45 -0700

    Test: craft class initializes

---
+++ spec/craft_spec.rb
@@ -0,0 +1,16 @@
+require './lib/person'
+require './lib/craft'
+
+RSpec.describe do
+  let(:person) {Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})}
+  let(:craft) {Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})}
+
+  it "exists" do
+    expect(craft).to be_a Craft
+  end
+
+  it "initializes" do
+    expect(craft.name).to eq('knitting')
+    expect(craft.supplies_required).to eq({yarn: 20, scissors: 1, knitting_needles: 2})
+  end
+end
