9469edb7af70c3cc92bd297059d68f8516009af6
oscar santos <oscar303data@gmail.com>
2022-01-11 09:31:20 -0700

    Test: class event is initialized with 3 arguments

---
+++ lib/event.rb
@@ -0,0 +1,13 @@
+class Event
+  attr_reader :name,
+              :crafts,
+              :attendees
+
+
+
+  def initialize(name, crafts, person)
+    @name = name
+    @crafts = crafts
+    @attendees = person
+  end
+end
