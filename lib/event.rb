4426ea1ffa62d60b313553b41dc61de95fb013ec
Conor <>
2022-01-11 09:46:58 -0700

    Feat: Craft_with_most_supplies

--- lib/event.rb
+++ lib/event.rb
@@ -7,7 +7,12 @@ class Event
     @attendees = attendees
   end
 
-  
+  def craft_with_most_supplies
+    ordered = @crafts.sort_by do |craft|
+      craft.supplies_required.size
+    end
+    ordered[-1].name
+  end
 
 
 end
