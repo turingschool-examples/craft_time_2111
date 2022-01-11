d326a97e808d17820050889ce47200a800ba5242
Sam Smith <samlsmith424@gmail.com>
2022-01-11 09:59:08 -0800

    Refactor

--- lib/person.rb
+++ lib/person.rb
@@ -8,4 +8,16 @@ class Person
     @interests = attributes[:interests]
     @supplies = {}
   end
+
+  def add_supply(item, amount)
+    if @supplies[item].nil?
+      @supplies[item] = amount
+    else
+      @supplies[item] += amount
+    end
+  end
+
+  def can_build?(craft)
+    craft.supplies_required.size == @supplies.size
+  end
 end
