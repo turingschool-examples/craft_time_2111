562e1d54c0e84c0407d1380057f127d3a7e09458
Maximilian Wagner <maw3350@gmail.com>
2022-01-11 11:56:03 -0500

    test: finish first test for event.rb

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -6,3 +6,14 @@ require 'rspec'
 RSpec.describe Event do
 
   let(:event) {Event.new("Carla's Craft Connection", [craft], [person])}
+
+  context 'iteration I' do
+
+    it 'exists and has attributes' do
+
+      expect(event.name).to eq("Carla's Craft Connection")
+      expect (event.crafts).to eq([craft])
+      expect(event.attendees).to eq([person])
+    end
+  end
+end
