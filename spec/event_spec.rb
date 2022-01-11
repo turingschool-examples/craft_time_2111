b0583614d295850020b12fa69d3668e9c8357d88
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-11 10:18:21 -0600

    Add #initialize to Event class

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -4,16 +4,20 @@ require './lib/person'
 
 RSpec.describe Event do
   it 'exists' do
+    craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
     event = Event.new("Carla's Craft Connection", [craft], [person])
 
     expect(event).to be_instance_of Event
   end
 
   it 'initializes with a name, crafts, and attendees' do
+    craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
     event = Event.new("Carla's Craft Connection", [craft], [person])
 
     expect(event.name).to eq("Carla's Craft Connection")
-    expect(event.crafts).to eq(craft)
-    expect(event.attendees).to eq(person)
+    expect(event.crafts).to eq([craft])
+    expect(event.attendees).to eq([person])
   end
 end
