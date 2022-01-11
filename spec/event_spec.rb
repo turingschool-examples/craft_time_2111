f3fbd5bcb0d146a90278c92a856b609e5ae7d70c
oscar santos <oscar303data@gmail.com>
2022-01-11 09:30:38 -0700

    Feat: it exists and has attributes block is created

---
+++ spec/event_spec.rb
@@ -0,0 +1,21 @@
+require './lib/person'
+require './lib/craft'
+require './lib/event'
+
+RSpec.describe Event do
+  it 'exists' do
+    person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    event = Event.new("Carla's Craft Connection", [craft], [person])
+    expect(event).to be_a(Event)
+  end
+
+  it 'has attributes' do
+    person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    event = Event.new("Carla's Craft Connection", [craft], [person])
+    expect(event.name).to eq("Carla's Craft Connection")
+    expect(event.crafts).to eq([craft])
+    expect(event.attendees).to eq([person])
+  end
+end
