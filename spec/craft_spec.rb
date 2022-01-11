49eab13f85fc8320e87b3f8c6fc77b002dd0e7ae
benistta <92293363+benistta@users.noreply.github.com>
2022-01-11 09:18:46 -0700

    craft has a name

--- spec/craft_spec.rb
+++ spec/craft_spec.rb
@@ -7,4 +7,9 @@ RSpec.describe Craft do
     craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
     expect(craft).to be_instance_of(Craft)
   end
+
+  it 'has a name' do
+    craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    expect(craft.name).to eq("knitting")
+  end
 end
