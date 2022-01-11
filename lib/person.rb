d44ac269e6e8906f5f3ab46d8ceb20af4a7caf40
Maximilian Wagner <maw3350@gmail.com>
2022-01-11 12:19:43 -0500

    feat: add #add_supply method, it works, hooray!

--- lib/person.rb
+++ lib/person.rb
@@ -4,6 +4,10 @@ class Person
   def initialize(info)
     @name   = info[:name]
     @interests = info[:interests]
-    @supplies = {}
+    @supplies = Hash.new(0)
+  end
+
+  def add_supply(key, value)
+    @supplies[key] += value
   end
 end
