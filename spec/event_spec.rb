00c5a90115e886e0edbc6536e2f7583113ee9787
Greg Flaherty <gregoryjflaherty@gmaul.com>
2022-01-11 11:25:36 -0500

    Test: create attendee names and most supplies tests

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -32,4 +32,22 @@ RSpec.describe do Event
     event = Event.new("Carla's Craft Connection", [craft], [person])
     expect(event.attendees).to eq([person])
   end
+
+  it 'has attendees names' do
+    hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+    sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+    knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    event = Event.new("Carla's Craft Connection", [sewing, knitting], [hector, toni])
+    expect(event.attendee_names).to eq(["Hector", "Toni"])
+  end
+
+  it 'gives crafts w/ most supplies' do
+    hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+    sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+    knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    event = Event.new("Carla's Craft Connection", [sewing, knitting], [hector, toni])
+    expect(event.craft_with_most_supplies).to eq("sewing")
+  end
 end
