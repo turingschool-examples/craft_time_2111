eca66a39593cd981fb57e61a1c2593788eda3af4
Richard LaBrecque <RichardlaBreque@Gmail.com>
2022-01-11 11:50:19 -0500

    feat: passes many attendess check

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -2,7 +2,7 @@ require './lib/person'
 require './lib/craft'
 require './lib/event'
 RSpec.describe Event do
-  
+
   it 'exists' do
     person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
     craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
@@ -31,4 +31,14 @@ RSpec.describe Event do
     event = Event.new("Carla's Craft Connection", [craft], [person])
     expect(event.attendees).to eq([person])
   end
+
+  it 'has array of many attendees' do
+    hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+    sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+    knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    event = Event.new("Carla's Craft Connection", [sewing, knitting], [hector, toni])
+    expect(event.attendees).to eq([hector, toni])
+  end
+
 end
