71bfdb901c68946e1f3c6111a899229c83dfb98c
Sam Iavri <sammi80134@gmail.com>
2022-01-11 09:38:52 -0700

    Person has supplies

--- lib/person.rb
+++ lib/person.rb
@@ -4,6 +4,10 @@ class Person
   def initialize(attributes)
     @name = attributes[:name]
     @interests = attributes[:interests]
-    @supplies = {}
+    @supplies = Hash.new(0)
+  end
+
+  def add_supply(name, count)
+    supplies[name] += count
   end
 end
