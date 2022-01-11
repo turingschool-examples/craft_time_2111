d27d18fca3025bbfb9deffb082149e5edb0c4bee
Richard LaBrecque <RichardlaBreque@Gmail.com>
2022-01-11 11:30:18 -0500

    feat: event exists pass

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -5,10 +5,11 @@ RSpec.describe Event do
   before(:each) do
     person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
     craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
-    event = Event.new("Carla's Craft Connection", [craft], [person])
+    @event = Event.new("Carla's Craft Connection", [craft], [person])
   end
   it 'exists' do
 
     # binding.pry
-    expect(craft).to be_a(Craft)
+    expect(@event).to be_a(Event)
   end
+end
