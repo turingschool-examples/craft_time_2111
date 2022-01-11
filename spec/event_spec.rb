9bd8aba4f2cbecb007ba8fdd1750520593048c26
Greg Flaherty <gregoryjflaherty@gmaul.com>
2022-01-11 11:13:22 -0500

    Test: create it exists test

---
+++ spec/event_spec.rb
@@ -0,0 +1,25 @@
+require './lib/event'
+
+
+
+RSpec.describe do Event
+  it 'exists' do
+    event = Event.new("Carla's Craft Connection", [craft], [person])
+    expect(event).to be_an_instance_of(Event)
+  end
+
+  it 'has a name' do
+    event = Event.new("Carla's Craft Connection", [craft], [person])
+    expect(event).to be_an_instance_of(Event)
+  end
+
+  it 'exists' do
+    event = Event.new("Carla's Craft Connection", [craft], [person])
+    expect(event).to be_an_instance_of(Event)
+  end
+
+  it 'exists' do
+    event = Event.new("Carla's Craft Connection", [craft], [person])
+    expect(event).to be_an_instance_of(Event)
+  end
+end
