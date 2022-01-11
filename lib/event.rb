ca14d47ad85e458ab7a5fc9b3ef078fe89a2f5e2
Greg Flaherty <gregoryjflaherty@gmaul.com>
2022-01-11 11:42:25 -0500

    Feat: create craft w/ most supply method and sypply list method

--- lib/event.rb
+++ lib/event.rb
@@ -5,4 +5,24 @@ class Event
     @crafts = crafts
     @attendees = attendees
   end
+
+  def attendee_names
+    @attendees.map {|attendee| attendee.name}
+  end
+
+  def craft_with_most_supplies
+    craft_hash = {}
+    @crafts.each do |craft|
+      craft_hash[craft.name] = craft.supplies.keys.length
+    end
+    craft_hash.key(craft_hash.values.max)
+  end
+
+  def supply_list
+    supplies = @crafts.reduce([]) do |array, craft|
+      array << craft.supplies.keys
+      array
+    end
+    supply_strings = supplies.flatten.uniq!.map {|supply| supply.to_s}
+  end
 end
