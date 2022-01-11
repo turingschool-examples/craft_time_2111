af86b176b663a980ecda0b90934f5f17d037b20e
benistta <92293363+benistta@users.noreply.github.com>
2022-01-11 09:26:45 -0700

    event has a name

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -9,4 +9,11 @@ RSpec.describe Event do
     event = Event.new("Carla's Craft Connection", [craft], [person])
     expect(event).to be_instance_of(Event)
   end
+
+  it 'has a name' do
+    person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    event = Event.new("Carla's Craft Connection", [craft], [person])
+    expect(event.name).to eq("Carla's Craft Connection")
+  end
 end
