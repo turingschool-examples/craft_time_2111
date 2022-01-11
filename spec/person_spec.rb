6060cda3974c2048c2fcbef279cdd1cfbc52f414
Jacob Jenner <jennerjacob@gmail.com>
2022-01-11 09:20:26 -0700

    Initial commit build first few tests and initialize person class

---
+++ spec/person_spec.rb
@@ -0,0 +1,20 @@
+require './lib/person'
+require './lib/craft'
+require './lib/event'
+
+RSpec.describe Person do
+  before(:each) do
+    @person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+  end
+
+  it 'exists' do
+    expect(@person).to be_a_instance_of(Person)
+  end
+
+  it 'has attributes' do
+    expect(@person.name).to eq("Hector")
+    expect(@person.interests).to eq(['sewing', 'millinery', 'drawing'])
+    expect(@person.supplies).to eq({})
+  end
+
+end
