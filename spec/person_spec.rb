1574606c65808b56331b63d3b3c399ac72bd0675
Greg Flaherty <gregoryjflaherty@gmaul.com>
2022-01-11 12:08:17 -0500

    TEst: create can have enough supplies test

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -49,4 +49,18 @@ RSpec.describe do Person
     hector.can_build?(sewing)
     expect(hector.can_build?(sewing)).to eq(false)
   end
+
+  it 'can have enough supplies' do
+    hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+    sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+    knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    event = Event.new("Carla's Craft Connection", [sewing, knitting], [hector, toni])
+    hector.add_supply('fabric', 7)
+    hector.add_supply('thread', 1)
+    hector.add_supply('scissors', 1)
+    hector.add_supply('sewing_needles', 1)
+    hector.can_build?(sewing)
+    expect(hector.can_build?(sewing)).to eq(true)
+  end
 end
