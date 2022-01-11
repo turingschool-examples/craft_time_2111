90c6dd12fca46e686fd384389492358ae99cf394
Lisa Taylor <ltaylor.lisa@gmail.com>
2022-01-11 09:39:32 -0700

    Add: attendee_names is passing test

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -19,3 +19,17 @@ RSpec.describe Event do
     expect(@event.attendees).to eq([@person])
   end
 end
+
+RSpec.describe Event do
+  before(:each) do
+    @hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    @toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+    @sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+    @knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    @event = Event.new("Carla's Craft Connection", [@sewing, @knitting], [@hector, @toni])
+  end
+
+  it '#attendee names' do
+    expect(@event.attendee_names).to eq(["Hector", "Toni"])
+  end
+end
