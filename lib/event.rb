eb1a90377dcbab3221d64dd89331929bba44a3e9
Greg Flaherty <gregoryjflaherty@gmaul.com>
2022-01-11 12:25:08 -0500

    Feat: create attendees by craft test and method

--- lib/event.rb
+++ lib/event.rb
@@ -25,4 +25,16 @@ class Event
     end
     supply_strings = supplies.flatten.uniq!.map {|supply| supply.to_s}
   end
+
+  def attendees_by_craft_interest
+    interest_hash = Hash.new { |hash, key| hash[key] = [] }
+    @crafts.each {|craft| interest_hash[craft.name] = [] }
+    @crafts.each do |craft|
+      @attendees.each do |person|
+        interest_hash[craft.name] << person if person.interests.include?(craft.name)
+      end
+    end
+    interest_hash
+    # require "pry"; binding.pry
+  end
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -59,4 +59,19 @@ RSpec.describe do Event
     event = Event.new("Carla's Craft Connection", [sewing, knitting], [hector, toni])
     expect(event.supply_list).to eq(["fabric", "scissors", "thread", "sewing_needles", "yarn", "knitting_needles"])
   end
+
+  it 'gives attendees by interest' do
+    hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+    tony = Person.new({name: 'Tony', interests: ['drawing', 'knitting']})
+    knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1})
+    painting = Craft.new('painting', {canvas: 1, paint_brush: 2, paints: 5})
+    event = Event.new("Carla's Craft Connection", [knitting, painting, sewing], [hector, toni, tony])
+    expect(event.attendees_by_craft_interest).to eq({
+      'knitting' => [toni, tony],
+      'painting' => [],
+      'sewing' => [hector, toni]
+      })
+  end
 end
