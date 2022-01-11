26268539e244cd0e3e271c38c3f2cad86e64c199
benistta <92293363+benistta@users.noreply.github.com>
2022-01-11 09:28:36 -0700

    event has crafts

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -16,4 +16,11 @@ RSpec.describe Event do
     event = Event.new("Carla's Craft Connection", [craft], [person])
     expect(event.name).to eq("Carla's Craft Connection")
   end
+
+  it 'has crafts' do
+    person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    event = Event.new("Carla's Craft Connection", [craft], [person])
+    expect(event.crafts).to eq([craft])
+  end
 end
