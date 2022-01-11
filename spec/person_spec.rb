6e959322d6e4296cd914cc49bac6dea614fa13dd
oscar santos <oscar303data@gmail.com>
2022-01-11 09:11:17 -0700

    Feat: it exists and has attributes is created for class person

---
+++ spec/person_spec.rb
@@ -0,0 +1,15 @@
+require './lib/person'
+
+RSpec.describe Person do
+  it 'exists' do
+    person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    expect(person).to be_a(Person)
+  end
+
+  it 'has attributes' do
+    person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    expect(person.name).to eq("Hector")
+    expect(person.interests).to eq( ["sewing", "millinery", "drawing"])
+    expect(person.supplies).to eq({})
+  end
+end
