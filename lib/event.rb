7c10bacff26d137f7b82a665f068c1836e785d69
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-11 10:54:35 -0600

    Add #supply_list to return list of all supplies required for event

--- lib/event.rb
+++ lib/event.rb
@@ -20,9 +20,20 @@ class Event
     @crafts.each do |craft|
       if craft.supplies_required.count > supplies_required
         (supplies_required = craft.supplies_required.count) && (craft_with_most_supplies = craft.name)
-      end 
+      end
     end
     craft_with_most_supplies
   end
+
+  def supply_list
+    supply_list = []
+    @crafts.each do |craft|
+      craft.supplies_required.each_key do |item|
+        supply_list.include?(item) ? next : supply_list << item.to_s
+      end
+    end
+    supply_list
+  end
+
 
 end
