3df00a35b7e4bd2ab439a78122ce6faf03269a25
Richard LaBrecque <RichardlaBreque@Gmail.com>
2022-01-11 11:15:39 -0500

    feat: pass empty supplies test

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,9 @@
 class Person
-  attr_reader :name, :interests
+  attr_reader :name, :interests, :supplies
   def initialize(name_interests_hash)
     @name_interests_hash = name_interests_hash
     @name = @name_interests_hash[:name]
     @interests = @name_interests_hash[:interests]
+    @supplies = {}
   end
 end
