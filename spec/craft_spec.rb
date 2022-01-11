ef70ae76dcc3e9e30e4241ff07c5c0e49d564f8d
Michael Hicks <hicks.michael04@gmail.com>
2022-01-11 09:32:32 -0700

    test: add supplies_required

--- spec/craft_spec.rb
+++ spec/craft_spec.rb
@@ -1,18 +1,18 @@
-require './lib/person'
 require './lib/craft'
-require './lib/event'
 require 'pry'
 
 RSpec.describe Craft do
-  describe 'Iteration 1' do
-    let(:craft) {Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})}
+  before(:each) do
+    @craft = Craft.new('knitting',{yarn: 20, scissors: 1, knitting_needles: 2})
+  end
 
-    it "exists" do
-      expect(craft).to be_a(Craft)      
-    end
+  it "exists" do
+    expect(@craft).to be_a(Craft)
+  end
 
-
-
+  it "has attributes" do
+    expect(@craft.name).to eq('knitting')
+    expect(@craft.supplies_required).to eq({:yarn=>20, :scissors=>1, :knitting_needles=>2})
   end
 
 
