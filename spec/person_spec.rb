9ffcb72f618a28388f61e1f65f32f9dc7436f6c3
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-01-11 09:13:13 -0700

    Test: Person class passes has name, can get interests, can initialize empty hash

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -10,4 +10,17 @@ RSpec.describe Person do
   it 'is an instance of Person' do
     expect(@person).to be_instance_of(Person)
   end
+
+  it 'can access name from inputted hash' do
+    expect(@person.name).to eq("Hector")
+  end
+
+  it 'can access interests array' do
+    expected_array = ["sewing", "millinery", "drawing"]
+    expect(@person.interests).to eq(expected_array)
+  end
+
+  it 'can initialize empty supplies hash' do
+    expect(@person.supplies).to eq({})
+  end
 end
