2c644ba5c32cc0173871a0b85b9f95759dfffbf0
Maximilian Wagner <maw3350@gmail.com>
2022-01-11 11:10:59 -0500

    test:innitial commit, create test for person class

---
+++ spec/person_spec.rb
@@ -0,0 +1,16 @@
+require './lib/person'
+require 'rspec'
+
+RSpec.describe Person do
+
+  let(:person) {Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})}
+
+  context 'iteration I' do
+
+    it 'exists and in instantiated with an attributes hash'
+
+    expect(person.name).to eq('Hector')
+    expect(person.interests).to eq(["sewing", "millinery", "drawing"])
+    expect(person.supplies).to eq({})
+  end
+end
