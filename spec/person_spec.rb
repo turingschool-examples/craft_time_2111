aac0a19f05f156a0832790382a4c71da4356bbf0
benistta <92293363+benistta@users.noreply.github.com>
2022-01-11 10:12:05 -0700

    hector can build still false

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -48,4 +48,17 @@ RSpec.describe Person do
     person.add_supply('fabric', 3)
     expect(hector.can_build?(sewing)).to eq(false)
   end
+
+  it 'hector can build is still false' do
+    sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+    knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    person.add_supply('fabric', 4)
+    person.add_supply('scissors', 1)
+    person.add_supply('fabric', 3)
+    hector.add_supply('fabric', 7)
+    hector.add_supply('thread', 1)
+    expect(hector.can_build?(sewing)).to eq(false)
+  end
 end
