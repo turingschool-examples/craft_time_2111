d8c68c2451886d3d96119f0f5b95e8f1bca0a307
Adrian Campos <adriancampos2992@yahoo.com>
2022-01-11 08:18:19 -0800

    Feature: add person has name test and method

--- lib/person.rb
+++ lib/person.rb
@@ -3,4 +3,8 @@ class Person
   def initialize(argument)
     @argument = argument
   end
+
+  def name
+    @argument[:name]
+  end
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -9,5 +9,9 @@ RSpec. describe do Person
     expect(person).to be_an_instance_of(Person)
   end
 
+  it 'person has a name' do
+    person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    expect(person.name).to eq("Hector")
+  end
 
 end
