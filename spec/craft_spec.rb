58d3b1fd4d8a6981e0417b712f28a18db1641326
Richard LaBrecque <RichardlaBreque@Gmail.com>
2022-01-11 11:22:28 -0500

    test: craft requires supplies test

--- spec/craft_spec.rb
+++ spec/craft_spec.rb
@@ -11,4 +11,10 @@ RSpec.describe Craft do
     # binding.pry
     expect(craft.name).to eq("knitting")
   end
+
+  it 'has required supplies' do
+    craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    # binding.pry
+    expect(craft.supplies_required).to eq({:yarn=>20, :scissors=>1, :knitting_needles=>2})
+  end
 end
