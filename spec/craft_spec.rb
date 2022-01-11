4659d6739f52748e57ce2f426bd549568f493c3d
Tamara Dowis <tamara.dowis@gmail.com>
2022-01-11 10:23:59 -0600

    test: Craft has attributes

--- spec/craft_spec.rb
+++ spec/craft_spec.rb
@@ -8,4 +8,9 @@ RSpec.describe Craft do
     expect(craft).to be_a Craft
   end
 
+  it 'has attributes' do
+    expect(craft.name).to eq("knitting")
+    expect(craft.supplies_required).to eq({:yarn=>20, :scissors=>1, :knitting_needles=>2})
+  end
+
 end
