47248c01576bc5e407b503e27d14a0f835698152
Richard LaBrecque <RichardlaBreque@Gmail.com>
2022-01-11 11:13:11 -0500

    fest: pass has interests test

--- lib/person.rb
+++ lib/person.rb
@@ -3,5 +3,6 @@ class Person
   def initialize(name_interests_hash)
     @name_interests_hash = name_interests_hash
     @name = @name_interests_hash[:name]
+    @interests = @name_interests_hash[:interests]
   end
 end
