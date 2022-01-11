0e631c477386b42ff4736d37b99df08b25ac2dad
Greg Flaherty <gregoryjflaherty@gmaul.com>
2022-01-11 11:04:17 -0500

    Test: create, name, interests and supplies test

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -7,4 +7,19 @@ RSpec.describe do Person
     person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
     expect(person).to be_an_instance_of(Person)
   end
+
+  it 'has a name' do
+    person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    expect(person.name).to eq(Hector)
+  end
+
+  it 'has interests' do
+    person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    expect(person.interests).to eq(["sewing", "millinery", "drawing"])
+  end
+
+  it 'has supplies' do
+    person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    expect(person.supplies).to eq({})
+  end
 end
