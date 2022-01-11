eb3edae60005f76ba0044bac5fd2b17031559da3
Lisa Taylor <ltaylor.lisa@gmail.com>
2022-01-11 09:25:12 -0700

    Add: event exists with passing test

---
+++ lib/event.rb
@@ -0,0 +1,11 @@
+class Event
+  attr_reader :name,
+              :crafts,
+              :attendees
+
+  def initialize(name, crafts, attendees)
+    @name = name
+    @crafts = crafts
+    @attendees = attendees
+  end
+end
---
+++ spec/event_spec.rb
@@ -0,0 +1,15 @@
+require './lib/person'
+require './lib/craft'
+require './lib/event'
+
+RSpec.describe Event do
+  before(:each) do
+    @person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    @craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    @event = Event.new("Carla's Craft Connection", [@craft], [@person])
+  end
+
+  it 'exists' do
+    expect(@event).to be_instance_of Event
+  end
+end
