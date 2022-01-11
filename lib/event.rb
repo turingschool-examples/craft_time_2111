f4c3d58af4cf3db575506aeeeae17eedeede46ca
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-11 10:41:46 -0600

    Add Event#craft_with_most_supplies to return the craft with the most required supplies

--- lib/event.rb
+++ lib/event.rb
@@ -11,7 +11,18 @@ class Event
   def attendee_names
     names = @attendees.map do |attendee|
       attendee.name
-    end 
+    end
+  end
+
+  def craft_with_most_supplies
+    supplies_required = 0
+    craft_with_most_supplies = nil
+    @crafts.each do |craft|
+      if craft.supplies_required.count > supplies_required
+        (supplies_required = craft.supplies_required.count) && (craft_with_most_supplies = craft.name)
+      end 
+    end
+    craft_with_most_supplies
   end
 
 end
