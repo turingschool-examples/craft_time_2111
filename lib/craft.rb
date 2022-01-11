2a06aae864e1b62df198ff6b98904410c6aba774
Adrian Campos <adriancampos2992@yahoo.com>
2022-01-11 09:00:00 -0800

    Feature: add craft has required supplies test and method

--- lib/craft.rb
+++ lib/craft.rb
@@ -2,9 +2,14 @@ class Craft
 
   def initialize(hobby, supplies)
     @hobby = hobby
+    @supplies = supplies
   end
 
   def name
     @hobby
+  end
+
+  def supplies_required
+    @supplies
   end
 end
--- spec/craft_spec.rb
+++ spec/craft_spec.rb
@@ -14,5 +14,10 @@ RSpec. describe do Craft
     expect(craft.name).to eq("knitting")
   end
 
+  it 'craft has required supplies' do
+    craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    expect(craft.supplies_required).to eq({:yarn=>20, :scissors=>1, :knitting_needles=>2})
+  end
+
 
 end
