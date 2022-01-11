9c2e350ebf5ea6f85aa75ec25073eb1d765212e6
oscar santos <oscar303data@gmail.com>
2022-01-11 10:25:54 -0700

    Test: method can_build? pass functionality test

--- lib/person.rb
+++ lib/person.rb
@@ -16,4 +16,21 @@ class Person
       @supplies[supply] += quantity
     end
   end
+
+  def can_build?(craft)
+    result = []
+   craft.supplies_required.map do |supply, value|
+     if !@supplies.keys.include?(supply.to_s)
+       result << false
+     else
+       result << true
+     end
+   end
+
+   if result.include?(false)
+     false
+   else
+     true
+   end
+  end
 end
