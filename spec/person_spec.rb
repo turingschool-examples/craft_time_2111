e942dcad39e75bb2371536205362f46579f4d2f0
Sam Iavri <sammi80134@gmail.com>
2022-01-11 09:08:11 -0700

    Person exists

---
+++ spec/person_spec.rb
@@ -0,0 +1,23 @@
+require './lib/person'
+
+RSpec.describe do
+  it 'exists' do
+    person = Person.new({ name: 'Hector', interests: %w[sewing millinery drawing] })
+    expect(person).to be_a(Person)
+  end
+
+  xit 'has a name' do
+    person = Person.new({ name: 'Hector', interests: %w[sewing millinery drawing] })
+    expect(person.name).to eq('Hector')
+  end
+
+  xit 'interests' do
+    person = Person.new({ name: 'Hector', interests: %w[sewing millinery drawing] })
+    expect(person.interests).to eq(%w[sewing millinery drawing])
+  end
+
+  xit 'no supplies' do
+    person = Person.new({ name: 'Hector', interests: %w[sewing millinery drawing] })
+    expect(person.supplies).to eq({})
+  end
+end
