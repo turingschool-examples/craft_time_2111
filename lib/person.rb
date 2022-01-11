62cafedfcd0afd79e47e8f13c69d2cdbb3836309
Tamara Dowis <tamara.dowis@gmail.com>
2022-01-11 11:42:54 -0600

    Feat: person add supplies

--- lib/person.rb
+++ lib/person.rb
@@ -4,10 +4,33 @@ class Person
   def initialize(attributes)
     @name = attributes.fetch(:name)
     @interests = attributes.fetch(:interests)
-    @supplies = {}
+    @supplies = Hash.new(0)
   end
 
-  def add_supply(object, how_many)
-    @supplies[object] = (how_many)
+  def add_supply(object, count)
+      @supplies[object] += count
+    end
   end
-end
+
+
+
+
+
+
+#
+#     [object] ||= (how_many)
+#   end
+# end
+# add_away_games = ab.each do |game|
+#       if games_hash.has_key?(game.away_team_id) ; games_hash[game.away_team_id] << game
+#     end
+#   end
+# # wins_hashed = {}
+#   games_hashed_by_opponent.each do |opponent, games|
+#     wins_hashed[opponent] ||= []
+#     games.select do |game|
+#                     wins_hashed[opponent] <<  game.away_goals
+#                     wins_hashed[opponent] <<  game.away_team_id
+#                     wins_hashed[opponent] <<  game.home_goals
+#                     wins_hashed[opponent] <<  game.home_team_id
+#                   end.flatten
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -18,7 +18,8 @@ RSpec.describe Person do
   it 'can add supplies' do
     person.add_supply('fabric', 4)
     person.add_supply('scissors', 1)
+    person.add_supply('fabric', 3)
 
-    expect(person.supplies).to eq({"fabric"=>4, "scissors"=>1})
+    expect(person.supplies).to eq({"fabric"=>7, "scissors"=>1})
   end
 end
