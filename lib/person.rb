80561e326b221c53e40a5a3b5d3efa716ecd9145
Jesse Ochs <91091690+JesseOchs@users.noreply.github.com>
2022-01-11 10:28:28 -0700

    Feat: one ugly way to add values

--- lib/person.rb
+++ lib/person.rb
@@ -6,4 +6,28 @@ class Person
     @supplies = {}
   end
 
+  def add_supply(name, cost)
+    x = { name => cost}
+    if @supplies.has_key?(name)
+        @supplies.merge!(x) { |key, old, new| if key == key
+        old + new
+      end}
+    else
+      create_hash(name, cost)
+    end
+    return @supplies
+  end
+
+  def create_hash(name, cost)
+    @supplies[name] = cost
+  end
+
 end
+
+
+
+
+
+# @supplies.merge(@supplies) { |key, old, new| if key == key
+#    old + new
+#  end }
