c8c73b588b8236dfd7a3f0d3d4f66c139bb5c95c
Tyler Ross <tross020890@gmail.com>
2022-01-11 10:54:55 -0600

    Feat: person adds supplies

--- lib/person.rb
+++ lib/person.rb
@@ -1,14 +1,22 @@
 class Person
-  attr_reader :info
+  attr_reader :supplies
   def initialize(info)
     @info = info
+    @supplies = {}
   end
 
   def name
-    info[:name]
+    @info[:name]
   end
 
   def interests
-    info[:interests]
+    @info[:interests]
+  end
+
+  def add_supply(supply, count)
+
+    @supplies[supply]
+    @supplies[supply] = count
   end
 end
+#require "pry"; binding.pry
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -18,4 +18,17 @@ RSpec.describe Person do
 
     expect(person.interests).to eq(["sewing", "millinery", "drawing"])
   end
+
+  it "has supplies" do
+    person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+
+    expect(person.supplies).to be_instance_of(Hash)
+  end
+
+    it "can add supplies" do
+      person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+      person.add_supply('fabric', 4)
+      person.add_supply('scissors', 1)
+      expect(person.supplies).to eq({"fabric"=>4, "scissors"=>1})
+    end
 end
