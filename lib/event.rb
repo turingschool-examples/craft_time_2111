3f2b121175e1237f0a38d0c2661913cf4deea916
Sam Smith <samlsmith424@gmail.com>
2022-01-11 08:58:03 -0800

    Feat: #craft_with_most_supplies and #supply_list

---
+++ lib/event.rb
@@ -0,0 +1,31 @@
+class Event
+  attr_reader :name,
+              :crafts,
+              :attendees
+
+  def initialize(name, crafts, attendees)
+    @name = name
+    @crafts = crafts
+    @attendees = attendees
+  end
+
+  def attendee_names
+    @attendees.map do |attendee|
+      attendee.name
+    end
+  end
+
+  def craft_with_most_supplies
+    @crafts.max_by do |craft|
+      craft.supplies_required.size
+    end.name
+  end
+
+  def supply_list
+    @crafts.map do |craft|
+      craft.supplies_required.keys.map do |key|
+        key.to_s
+      end
+    end.flatten.uniq  
+  end
+end
