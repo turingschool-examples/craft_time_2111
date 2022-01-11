e84e2f58d8875d037f7556e2bed964604f92a633
Sam Iavri <sammi80134@gmail.com>
2022-01-11 09:19:37 -0700

    Craft exists with a name and supplies required

---
+++ spec/craft_spec.rb
@@ -0,0 +1,18 @@
+require './lib/craft'
+
+RSpec.describe do
+  it 'exists' do
+    craft = Craft.new('knitting', { yarn: 20, scissors: 1, knitting_needles: 2 })
+    expect(craft).to be_a(Craft)
+  end
+
+  it 'has a name' do
+    craft = Craft.new('knitting', { yarn: 20, scissors: 1, knitting_needles: 2 })
+    expect(craft.name).to eq('knitting')
+  end
+
+  it 'interests' do
+    craft = Craft.new('knitting', { yarn: 20, scissors: 1, knitting_needles: 2 })
+    expect(craft.supplies_required).to eq({ yarn: 20, scissors: 1, knitting_needles: 2 })
+  end
+end
