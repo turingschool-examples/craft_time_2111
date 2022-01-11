f78c5bac00976b4c0366eb72e9a03c4479dbe0d2
Richard LaBrecque <RichardlaBreque@Gmail.com>
2022-01-11 11:20:06 -0500

    test: has a name test

--- spec/craft_spec.rb
+++ spec/craft_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe Craft do
     # binding.pry
     expect(craft).to be_a(Craft)
   end
+
+  it 'has a name' do
+    craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    # binding.pry
+    expect(craft.name).to be_a("knitting")
+  end
 end
