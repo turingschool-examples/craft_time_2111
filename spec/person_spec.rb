13af0b5763f24307ce61de64f1a7eb066b5ade89
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-11 10:05:17 -0600

    Test: Person initializes with name, interests, and no supplies

---
+++ spec/person_spec.rb
@@ -0,0 +1,17 @@
+require './lib/person'
+
+RSpec.describe StatTracker do
+  it 'exists' do
+    person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+
+    expect(person).to be_instance_of Person
+  end
+
+  it 'initializes with a name, interests, and no supplies' do
+    person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+
+    expect(person.name).to eq("Hector")
+    expect(person.interests).to eq(["sewing", "millinery", "drawing"])
+    expect(person.supplies).to eq({})
+  end
+end
