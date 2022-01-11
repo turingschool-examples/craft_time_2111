1606112956ee20fa6a89ba8cd702814a844a55af
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-11 11:20:04 -0600

    Test: Event can return list of attendees by craft interests

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -41,7 +41,7 @@ RSpec.describe Event do
     expect(event.craft_with_most_supplies).to eq("sewing")
   end
 
-  it 'can return the supply list required for all crafts in event' do
+  xit 'can return the supply list required for all crafts in event' do
     hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
     toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
     sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
@@ -49,5 +49,23 @@ RSpec.describe Event do
     event = Event.new("Carla's Craft Connection", [sewing, knitting], [hector, toni])
 
     expect(event.supply_list).to eq(["fabric", "scissors", "thread", "sewing_needles", "yarn", "knitting_needles"])
+  end
+
+  it 'can return a list of attendees by craft interests' do
+    hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+    tony = Person.new({name: 'Tony', interests: ['drawing', 'knitting']})
+    knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1})
+    painting = Craft.new('painting', {canvas: 1, paint_brush: 2, paints: 5})
+    event = Event.new("Carla's Craft Connection", [knitting, painting, sewing], [hector, toni, tony])
+
+    expected = {
+      "knitting" => [toni, tony],
+      "painting" => [],
+      "sewing" => [hector, toni]
+    }
+
+    expect(event.attendees_by_craft_interest).to eq(expected)
   end
 end
