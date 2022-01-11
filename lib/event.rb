1ce22a71f38815dd067394f1866f77ea5f926606
Austin Moore <austinchristianmoore@gmail.com>
2022-01-11 12:19:06 -0500

    Feature: Pass it1 tests

--- lib/event.rb
+++ lib/event.rb
@@ -1,11 +1,21 @@
 class Event
   attr_reader :name,
-              :craft,
-              :person
-
+              :crafts,
+              :attendees
+  # require "pry" ; binding.pry
+  @@craft
   def initialize(name, craft, person)
     @name = name
-    @@crafts = [craft]
-    @@person = [person]
+    # require "pry" ; binding.pry
+    @crafts = craft
+    @attendees = person
   end
+
+  # def self.access_crafts(craft)
+  #   require "pry" ; binding.pry
+  # end
+  #
+  # def self.access_attendees
+  #   attendees = [@@person]
+  # end
 end
